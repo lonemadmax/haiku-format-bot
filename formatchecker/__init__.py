ff2321df0fc4d1a1c67016aa4d71d8acf2572fed
Niels Sascha Reedijk <niels.reedijk@gmail.com>
2024-01-14 15:52:50 +0000

    Set __version__ to 0.1

---
+++ formatchecker/__init__.py
@@ -0,0 +1,9 @@
+#
+# Copyright 2024 Haiku, Inc. All rights reserved.
+# Distributed under the terms of the MIT License.
+#
+# Authors:
+#  Niels Sascha Reedijk, niels.reedijk@gmail.com
+#
+
+__version__ = '0.1'
